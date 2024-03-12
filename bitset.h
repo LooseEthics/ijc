f27c2d97e3e8747c4c8f6967f31b15184d792d88
LooseEthics <36419655+LooseEthics@users.noreply.github.com>
2024-03-12 21:25:38 +0100

    long

--- bitset.h
+++ bitset.h
@@ -5,11 +5,11 @@
 typedef unsigned long bitset_index_t;
 
 typedef struct {
-    bitset_index_t size; // size in bits, not bytes
-    char* bytes;
+    unsigned long* stuff;
 } bitset_t;
 
-#define BIT_TO_BYTE(size) ((size >> 3) + ((size & 0x7) != 0))
+// size long + full longs + final long
+#define BIT_TO_UL(size) (1 + (size / sizeof(unsigned long)) + ((size & (sizeof(unsigned long) - 1)) != 0))
 
 #if defined(_WIN32) || defined(_WIN64) // windows
 #define MALLOC_SIZE(ptr) _msize(ptr)
@@ -17,37 +17,41 @@ typedef struct {
 #define MALLOC_SIZE(ptr) malloc_usable_size(ptr)
 #endif
 
-// I'd rather wrap bitset_create and bitset_alloc inside a do-while, but then the structs don't persist
+// Can't put bitset_create and bitset_alloc inside a do-while, the structs don't persist
 // I guess just don't use these as the body of single line ifs and fors
 #define bitset_create(_name, _size) \
-  bitset_index_t _bitset_size = BIT_TO_BYTE(_size); \
-  char _bitset_bytes[_bitset_size]; \
-  for (unsigned i = 0; i < _bitset_size; i++) _bitset_bytes[i] = 0x0; \
-  bitset_t _name = {_size, _bitset_bytes};
+  bitset_index_t _bitset_size = BIT_TO_UL(_size); \
+  unsigned long _bitset_stuff[_bitset_size]; \
+  _bitset_stuff[0] = _size; \
+  for (unsigned i = 1; i < _bitset_size; i++) _bitset_stuff[i] = 0x0; \
+  bitset_t _name = {_bitset_stuff};
 
 #define bitset_alloc(_name, _size) \
-  bitset_index_t _bitset_size = BIT_TO_BYTE(_size); \
-  bitset_t _name = {_size, NULL}; \
-  _name.bytes = (char *)calloc(_bitset_size, sizeof(char)); \
+  bitset_index_t _bitset_size = BIT_TO_UL(_size); \
+  unsigned long* _bitset_stuff = (unsigned long *)calloc(_bitset_size, sizeof(unsigned long)); \
   assert(((void)"bitset_alloc: Chyba alokace paměti", \
-    _name.bytes != NULL && \
-    MALLOC_SIZE(_name.bytes) == BIT_TO_BYTE(_name.size) * sizeof(_name.bytes[0]) ));
+    _bitset_stuff != NULL && \
+    MALLOC_SIZE(_bitset_stuff) == BIT_TO_UL(_bitset_size) * sizeof(_bitset_stuff[0]) )); \
+  _bitset_stuff[0] = _size; \
+  bitset_t _name = {_bitset_stuff};
 
-#define bitset_free(_name) free(_name.bytes)
+#define bitset_free(_name) free(_name.stuff)
 
-#define bitset_size(_name) (_name.size)
+#define bitset_size(_name) (_name.stuff[0])
 
 #define bitset_fill(_name, _bool) \
   do { \
-    unsigned char _fill = (_bool) ? 0xff : 0x0; \
-    for (unsigned i = 0; i < BIT_TO_BYTE(_name.size); i++) _name.bytes[i] = _fill; \
+    unsigned long _fill = (_bool) ? 0x0 - 1 : 0x0; \
+    for (unsigned i = 1; i < BIT_TO_UL(_name.size); i++) _name.stuff[i] = _fill; \
   } while (0);
 
+//TODO assert boundary
 #define bitset_setbit(_name, _index, _bool) \
   do { \
-    unsigned char _mask = 0x1 << (_index % 8); \
-    bitset_index_t _byte_index = _index >> 3; \
-    _name.bytes[_byte_index] = (_bool) ? (_name.bytes[_byte_index] | _mask) : (_name.bytes[_byte_index] & (~_mask)); \
+    unsigned char _mask = 0x1 << (_index % sizeof(unsigned long)); \
+    bitset_index_t _byte_index = (_index / sizeof(unsigned long)) + 1; \
+    _name.stuff[_byte_index] = (_bool) ? (_name.stuff[_byte_index] | _mask) : (_name.stuff[_byte_index] & (~_mask)); \
   } while (0);
 
-#define bitset_getbit(_name, _index) (((_name.bytes[(_index >> 3)] & (0x1 << (_index % 8))) == 0) ? 0 : 1)
+//TODO assert boundary
+#define bitset_getbit(_name, _index) (((_name.stuff[(_index / sizeof(unsigned long))] & (0x1 << (_index % sizeof(unsigned long)))) == 0) ? 0 : 1)
--- main.c
+++ main.c
@@ -1,19 +1,23 @@
 #include "bitset.h"
 #include <stdio.h>
 
-void print_hex_char(char c){
-  char ntop = (c >> 4) & 0xf;
-  char htop = (ntop < 0x9) ? ntop + '0' : ntop + 'a' - 0xa;
-  char nbot = c & 0xf;
-  char hbot = (nbot < 0x9) ? nbot + '0' : nbot + 'a' - 0xa;
-  printf("%c%c ", htop, hbot);
+void print_hex_ul(char ul){
+  char nibble;
+  char hibble;
+  for (int i = 28; i > 0; i -= 4){
+    nibble = (ul >> i) & 0xf;
+    hibble = (nibble < 0x9) ? nibble + '0' : nibble + 'a' - 0xa;
+    printf("%c", hibble);
+  }
+  printf(" ");
 }
 
 void print_bs(bitset_t bs){
-  printf("Size: %i", bs.size);
-  for (unsigned i = 0; i < BIT_TO_BYTE(bs.size); i++){
+  printf("Size: %lu", bs.stuff[0]);
+  for (unsigned i = 1; i < BIT_TO_UL(bs.size); i++){
     if (!(i & 0x7)) printf("\n");
-    print_hex_char(bs.bytes[i]);
+    print_hex_ul(bs.stuff[i]);
+    //printf("%lu ", bs.stuff[i]);
   }
   printf("\n");
 }
