856c4993d57813345cf7e4027093b665f78a3bdf
LooseEthics <36419655+LooseEthics@users.noreply.github.com>
2024-03-13 13:13:24 +0100

    it works

--- bitset.h
+++ bitset.h
@@ -4,12 +4,14 @@
 
 typedef unsigned long bitset_index_t;
 
+#define LONG_BSIZE (sizeof(unsigned long) * 8)
+
 typedef struct {
     unsigned long* stuff;
 } bitset_t;
 
 // size long + full longs + final long
-#define BIT_TO_UL(size) (1 + (size / sizeof(unsigned long)) + ((size & (sizeof(unsigned long) - 1)) != 0))
+#define BIT_TO_UL(size) (1 + (size / LONG_BSIZE) + ((size & (sizeof(unsigned long) - 1)) != 0))
 
 #if defined(_WIN32) || defined(_WIN64) // windows
 #define MALLOC_SIZE(ptr) _msize(ptr)
@@ -21,6 +23,8 @@ typedef struct {
 // I guess just don't use these as the body of single line ifs and fors
 #define bitset_create(_name, _size) \
   bitset_index_t _bitset_size = BIT_TO_UL(_size); \
+  printf("_bitset_size %u\n", _bitset_size); \
+  printf("%u %u %u\n", 1, (_size / LONG_BSIZE), ((_size & (sizeof(unsigned long) - 1)) != 0)); \
   unsigned long _bitset_stuff[_bitset_size]; \
   _bitset_stuff[0] = _size; \
   for (unsigned i = 1; i < _bitset_size; i++) _bitset_stuff[i] = 0x0; \
@@ -41,17 +45,26 @@ typedef struct {
 
 #define bitset_fill(_name, _bool) \
   do { \
-    unsigned long _fill = (_bool) ? 0x0 - 1 : 0x0; \
-    for (unsigned i = 1; i < BIT_TO_UL(_name.size); i++) _name.stuff[i] = _fill; \
+    unsigned long _fill = (_bool) ? ~0x0 : 0x0; \
+    for (unsigned i = 1; i < BIT_TO_UL(_name.stuff[0]); i++) _name.stuff[i] = _fill; \
   } while (0);
 
-//TODO assert boundary
+//TODO somehow shove the formatting into the assert
 #define bitset_setbit(_name, _index, _bool) \
   do { \
-    unsigned char _mask = 0x1 << (_index % sizeof(unsigned long)); \
-    bitset_index_t _byte_index = (_index / sizeof(unsigned long)) + 1; \
+    assert(((void)"bitset_getbit: Index %lu mimo rozsah 0..%lu", \
+      _index < _name.stuff[0] \
+    )); \
+    unsigned long _mask = (0x1 << (LONG_BSIZE - 1 - _index)); \
+    bitset_index_t _byte_index = (_index / LONG_BSIZE) + 1; \
     _name.stuff[_byte_index] = (_bool) ? (_name.stuff[_byte_index] | _mask) : (_name.stuff[_byte_index] & (~_mask)); \
   } while (0);
 
-//TODO assert boundary
-#define bitset_getbit(_name, _index) (((_name.stuff[(_index / sizeof(unsigned long))] & (0x1 << (_index % sizeof(unsigned long)))) == 0) ? 0 : 1)
+//TODO somehow shove the formatting into the assert
+#define bitset_getbit(_name, _index) \
+  ( \
+    assert(((void)"bitset_getbit: Index %lu mimo rozsah 0..%lu", \
+      _index < _name.stuff[0] \
+    )), \
+    ((_name.stuff[(_index / LONG_BSIZE) + 1] & (0x1 << (LONG_BSIZE - 1 - _index))) != 0) \
+  )
--- main.c
+++ main.c
@@ -1,37 +1,57 @@
 #include "bitset.h"
 #include <stdio.h>
 
-void print_hex_ul(char ul){
+void print_hex_ul(unsigned long ul){
   char nibble;
   char hibble;
-  for (int i = 28; i > 0; i -= 4){
+  for (int i = 28; i >= 0; i -= 4){
     nibble = (ul >> i) & 0xf;
-    hibble = (nibble < 0x9) ? nibble + '0' : nibble + 'a' - 0xa;
+    hibble = (nibble <= 0x9) ? nibble + '0' : nibble + 'a' - 0xa;
     printf("%c", hibble);
   }
   printf(" ");
 }
 
+void print_bits(unsigned long ul){
+  for (int i = sizeof(unsigned long)*8 - 1; i >= 0; i--){
+    printf("%i", (ul & (0x1 << i)) != 0);
+  }
+  printf(" ");
+}
+
 void print_bs(bitset_t bs){
-  printf("Size: %lu", bs.stuff[0]);
-  for (unsigned i = 1; i < BIT_TO_UL(bs.size); i++){
-    if (!(i & 0x7)) printf("\n");
+  printf("Size: %lu\n", bs.stuff[0]);
+  print_bits(bs.stuff[0]);
+  for (unsigned i = 1; i < BIT_TO_UL(bs.stuff[0]); i++){
+    if (!((i - 1) & 0x3)) printf("\n");
+    print_bits(bs.stuff[i]);
+  }
+  printf("\n");
+  print_hex_ul(bs.stuff[0]);
+  for (unsigned i = 1; i < BIT_TO_UL(bs.stuff[0]); i++){
+    if (!((i - 1) & 0x3)) printf("\n");
     print_hex_ul(bs.stuff[i]);
-    //printf("%lu ", bs.stuff[i]);
   }
   printf("\n");
 }
 
 int main(){
-    bitset_create(a, 33);
+    bitset_index_t size = 241;
+    printf("bitset_create(a, %i);\n", size);
+    bitset_create(a, size);
     print_bs(a);
+    printf("bitset_fill(a, 1);\n");
     bitset_fill(a, 1);
     print_bs(a);
+    printf("bitset_fill(a, 0);\n");
     bitset_fill(a, 0);
     print_bs(a);
+    printf("bitset_setbit(a, 10, 1);\nbitset_setbit(a, 20, 1);\n");
     bitset_setbit(a, 10, 1);
     bitset_setbit(a, 20, 1);
     print_bs(a);
+    unsigned bit = (bitset_getbit(a, 10));
+    printf("%u\n", bit);
     bitset_free(a);
     return 0;
 }
